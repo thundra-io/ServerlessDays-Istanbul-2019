ff2bd854fbf417f49008abe0658ac006a5304d26
Serkan ÖZAL <serkan@thundra.io>
2019-07-24 18:49:12 +0300

    Added ticketing link

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ class Header extends Component {
           <img src={'./media/serverless-days-logo-transparent2.png'} alt={'ServerlessDays İstanbul logo'} className='responsive-img' />
           <h3>Talk Serverless at the heart of İstanbul.</h3>
           <h4>Save The Date</h4>
-          {/* <a href='https://www.eventbrite.com/e/serverlessdays-istanbul-tickets-???' target='_blank' rel='noopener noreferrer' className='btn-large top-marg'>Get Tickets!</a> */}
+          <iframe src= "https://biletino.com/ticketing/start/8131" style={{width: "100%", height: "400px"}}/>
           <h5 className='top-marg'>October 03, 2019 - İstanbul, Turkey</h5>
         </header>
       </div>
