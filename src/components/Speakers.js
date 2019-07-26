e78e9d17a85ff8a3f67636115e1a39d5bb206080
Serkan ÖZAL <serkan@thundra.io>
2019-07-26 13:40:09 +0300

    Added new speakers

--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -39,7 +39,90 @@ class Speakers extends Component {
           </Row>
           <div className='divider' />
           <div className='space-foot' />
-          {/* <p>Want to find out who's speaking as soon as they're announced? Sign up for our <a data-target='signupModal' className='modal-trigger'>mailing list</a> to be notified.</p> */}
+          <Row>
+            <Col s={12} l={12} id='daron'>
+              <h4 className='top-marg'>Mx. Kassian Perch</h4>
+              <h5 className='company'>Cloudflare</h5>
+            </Col>
+            <Col s={12} l={9}>
+              <p>Kas (they/them pronouns) is a Developer Advocate for Cloudflare by day, and a roboticist, musician, and eternal student by night. They love serverless, electronics, and discussion.</p>
+              <h5><a href='https://twitter.com/nodebotanist' target='_blank' rel='noopener noreferrer'><i className="fab fa-twitter light-blue-text text-darken-1"></i></a></h5>
+            </Col>
+            <Col s={12} l={3}>
+              <img src={'./media/speakers/kassian.jpg'} alt={'Mx. Kassian Perch'} className='responsive-img circle' />
+            </Col>
+          </Row>
+          <div className='space-foot' />
+          <Row>
+            <Col s={12} l={12} id='daron'>
+              <h4 className='top-marg'>Daron Yondem</h4>
+              <h5 className='company'>Microsoft</h5>
+            </Col>
+            <Col s={12} l={9}>
+              <p>Daron Yondem is a Microsoft Regional Director and an Azure MVP. He is a regular speaker at international conferences focusing on Microservices, Serverless, and IoT. Daron currently works as a CTO in XOMNI Inc, a cloud company building PAAS offerings for retailers and XOGO where they are building a decision signage platform.</p>
+              <h5><a href='https://twitter.com/daronyondem' target='_blank' rel='noopener noreferrer'><i className="fab fa-twitter light-blue-text text-darken-1"></i></a><a href='https://www.linkedin.com/in/daronyondem/' target='_blank' rel='noopener noreferrer'><i className="fab fa-linkedin-in light-blue-text text-darken-3"></i></a></h5>
+            </Col>
+            <Col s={12} l={3}>
+              <img src={'./media/speakers/daron.png'} alt={'Daron Yondem'} className='responsive-img circle' />
+            </Col>
+          </Row>
+          <div className='divider' />
+          <Row>
+            <Col s={12} l={12} id='mete'>
+              <h4 className='top-marg'>Mete Atamel</h4>
+              <h5 className='company'>Google</h5>
+            </Col>
+            <Col s={12} l={9}>
+              <p>Mete Atamel is a Developer Advocate at Google, focused on helping developers with Google Cloud. As a long-time Java and C# developer, he likes to compare the two ecosystems. Prior to Google, he worked at Microsoft, Skype, Adobe, EMC, and Nokia building apps and services on various web, mobile and cloud platforms. Originally from the island of Cyprus, he currently lives in Greenwich of London, not too far away from the prime meridian.</p>
+              <h5><a href='https://twitter.com/meteatamel' target='_blank' rel='noopener noreferrer'><i className="fab fa-twitter light-blue-text text-darken-1"></i></a><a href='https://www.linkedin.com/in/meteatamel/' target='_blank' rel='noopener noreferrer'><i className="fab fa-linkedin-in light-blue-text text-darken-3"></i></a></h5>
+            </Col>
+            <Col s={12} l={3}>
+              <img src={'./media/speakers/mete.jpg'} alt={'Mete Atamel'} className='responsive-img circle' />
+            </Col>
+          </Row>
+          <div className='divider' />
+          <Row>
+            <Col s={12} l={12} id='mete'>
+              <h4 className='top-marg'>Eric Johnson</h4>
+              <h5 className='company'>AWS</h5>
+            </Col>
+            <Col s={12} l={9}>
+              <p>Eric Johnson is a Senior Developer Advocate for Serverless Applications at Amazon Web Services and is based in Northern Colorado. Eric is a fanatic about serverless and enjoys helping developers understand how serverless technologies introduces a major paradigm shift in how they approach building and running applications at massive scale with minimal administration overhead. Prior to this, Eric has worked as a developer, solutions architect and AWS Evangelist for an AWS partner company.</p>
+              <h5><a href='https://twitter.com/edjgeek' target='_blank' rel='noopener noreferrer'><i className="fab fa-twitter light-blue-text text-darken-1"></i></a><a href='https://www.linkedin.com/in/singledigit/' target='_blank' rel='noopener noreferrer'><i className="fab fa-linkedin-in light-blue-text text-darken-3"></i></a></h5>
+            </Col>
+            <Col s={12} l={3}>
+              <img src={'./media/speakers/eric.jpg'} alt={'Eric Johnson'} className='responsive-img circle' />
+            </Col>
+          </Row>
+          <div className='divider' />
+          <Row>
+            <Col s={12} l={12} id='mete'>
+              <h4 className='top-marg'>Syed Sarjeel Yusuf</h4>
+              <h5 className='company'>Atlassian</h5>
+            </Col>
+            <Col s={12} l={9}>
+              <p>Syed Sarjeel Yusuf is a software engineer at Atlassian Opsgenie being a member of Infra team. He is a cloud citizen by heart. Before joining Atlassian Opsgenie, he worked in Thundra where he increased his knowledge about serverless architectures a lot. He is primarily focused on architecture and Atlassian Opsgenie with serverless architecture.</p>
+              <h5><a href='https://twitter.com/SarjeelY' target='_blank' rel='noopener noreferrer'><i className="fab fa-twitter light-blue-text text-darken-1"></i></a><a href='https://www.linkedin.com/in/syedsarj/' target='_blank' rel='noopener noreferrer'><i className="fab fa-linkedin-in light-blue-text text-darken-3"></i></a></h5>
+            </Col>
+            <Col s={12} l={3}>
+              <img src={'./media/speakers/sarjeel.jpg'} alt={'Syed Sarjeel Yusuf'} className='responsive-img circle' />
+            </Col>
+          </Row>
+          <div className='divider' />
+          <Row>
+            <Col s={12} l={12} id='mete'>
+              <h4 className='top-marg'>Mehmet Taşkıner</h4>
+              <h5 className='company'>Getir</h5>
+            </Col>
+            <Col s={12} l={9}>
+              <p>Mehmet Taşkıner is a DevOps engineer at Getir.</p>
+              <h5><a href='https://twitter.com/taskinermehmet' target='_blank' rel='noopener noreferrer'><i className="fab fa-twitter light-blue-text text-darken-1"></i></a><a href='https://www.linkedin.com/in/mehmettaskiner/' target='_blank' rel='noopener noreferrer'><i className="fab fa-linkedin-in light-blue-text text-darken-3"></i></a></h5>
+            </Col>
+            <Col s={12} l={3}>
+              <img src={'./media/speakers/mehmet.jpg'} alt={'Mehmet Taşkıner'} className='responsive-img circle' />
+            </Col>
+          </Row>
+          <div className='divider' />
         </div>
       </div>
     );
